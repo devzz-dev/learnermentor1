73ee9f1af1b92babbfb6ac9c7a842efea8a64918
devzz-dev <62280506+devzz-dev@users.noreply.github.com>
2020-03-18 11:43:59 +0530

    Add files via upload

---
+++ learnermentor.py
@@ -0,0 +1,29 @@
+class tech:
+    
+def __init__(self, name):
+         
+              self.name = name
+   
+def addStacks(self,expertise):
+        
+       self.expertise = expertise
+    
+def setMentorOrLearner(self,post):
+        
+       self.post = post
+    
+def setAvailableTime(self,time):
+        
+        if self.post in ['MENTOR']:
+            
+              self.time = time
+        
+        else:
+            
+              exit()
+    
+def getMentor(self,expertise,time):
+        
+        if self.expertise == expertise and self.time == time:
+            
+               print "Available Mentors are %d" % self.name
